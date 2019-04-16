4980bd39ae5966398b9ca1cba6d8e3b5827957e2
secrettttt <243950548@qq.com>
2019-04-16 23:13:09 +0800

    增加07-tool(txt_to_csv);修改04改04_MDS(更通用)

--- 04_MDS（multi-dimensional scaling）/MDS.py
+++ 04_MDS（multi-dimensional scaling）/MDS.py
@@ -6,38 +6,41 @@ Created on Thu Apr 11 19:44:56 2019
 """
 import csv
 import numpy as np
-import pandas as pd
 from sklearn.manifold import MDS
-import matplotlib.pyplot as plt
 
-data = np.zeros((1001, 1001))
+#输入：边集.csv
+#输出：点的坐标.csv
+#三处需要更改：1,2,3
 
+#1文件名
 with open('network_mix1.csv','r') as csvfile:
     reader = csv.reader(csvfile)
     x = [row[0] for row in reader]
-    
+ 
+#2文件名
 with open('network_mix1.csv','r') as csvfile:
     reader = csv.reader(csvfile)
     y = [row[-1] for row in reader] 
 
+xSet=set(x)
+xIter=list(xSet)
+for i in xIter:
+    if i.isalnum()==False:
+        xIter.remove(i)
+
+data = np.zeros((len(xIter), len(xIter)))
+
 for i in range(0,len(y)-1):
-    data[int(x[i])][int(y[i])]=1
-    
-x = [x for x in range(1,1001)]
-x.append(int(0))
-
-y = [x for x in range(1,1001)]
-y.append(int(0))
-
-Word = pd.DataFrame( data , x , y )
+    data[int(x[i])%len(xIter)][int(y[i])%len(xIter)]=1
 
 mds = MDS(dissimilarity='precomputed')
 
 point = mds.fit_transform(data)
 
-csvFile = open('network_mix1_point.csv','w', newline='') # 设置newline，否则两行之间会空一行
+#3文件名
+csvFile = open('network_mix1_point.csv','w', newline='')#设置newline，否则两行之间会空一行
 writer = csv.writer(csvFile)
-for i in range(1,len(point)):#从1开始是因为第一个点（0,0）是前面加上去的，没卵用
+for i in range(0,len(xIter)):
     writer.writerow(point[i])
 csvFile.close()
 
